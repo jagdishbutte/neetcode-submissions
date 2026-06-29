b764b3f1b733fa30b183ee657c7465a454e64588
JagdishButte <86142807+jagdishbutte@users.noreply.github.com>
2026-06-29 20:34:50 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // int[] ans = new int[2];
+        // outer:for(int i = 0; i < nums.length; i++) {
+        //     inner:for(int j = 0; j < nums.length; j++) {
+        //         if(i != j && nums[i] + nums[j] == target) {
+        //             ans[0] = i;
+        //             ans[1] = j;
+        //             break outer;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // int[][] elements = new int[nums.length][2];
+        // for(int i = 0; i < nums.length; i++) {
+        //     elements[i][0] = nums[i];
+        //     elements[i][1] = i;
+        // }
+        // Arrays.sort(elements, (a, b) -> Integer.compare(a[0], b[0]));
+        // int left = 0;
+        // int right = elements.length - 1;
+        // while(left < right) {
+        //     int cur = elements[left][0] + elements[right][0];
+        //     if(cur == target) {
+        //         return new int[] {Math.min(elements[left][1], elements[right][1]), Math.max(elements[left][1], elements[right][1])};
+        //     } else if(target > cur) {
+        //         left++;
+        //     } else {
+        //         right--;
+        //     }
+        // }
+        // return new int[0];
+
+        HashMap<Integer, Integer> indices = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            indices.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(indices.containsKey(diff) && indices.get(diff) != i) {
+                return new int[] {i, indices.get(diff)};
+            }
+        }
+
+        return new int[0];
+    }
+}
