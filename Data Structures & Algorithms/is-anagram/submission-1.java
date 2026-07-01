910c99fcc121ef21f0a1728fa3b09b7ee3ce0607
JagdishButte <86142807+jagdishbutte@users.noreply.github.com>
2026-07-01 23:03:34 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        // char[] sarr = s.toCharArray();
+        // char[] tarr = t.toCharArray();
+        // Arrays.sort(sarr);
+        // Arrays.sort(tarr);
+        // return Arrays.equals(sarr, tarr);
+
+        HashMap<Character, Integer> Count1 = new HashMap<>();
+        HashMap<Character, Integer> Count2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            Count1.put(s.charAt(i), Count1.getOrDefault(s.charAt(i), 0) + 1);
+            Count2.put(t.charAt(i), Count2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return Count1.equals(Count2);
+    }
+}
